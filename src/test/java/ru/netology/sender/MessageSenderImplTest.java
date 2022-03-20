1ded0ae1e1f2d4fe3ed8a5874ee9579afc369c99
vakhrushevkirill <vakhrushev.kirill93@yandex.ru>
2022-03-20 16:49:56 +0300

    En test

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -15,16 +15,29 @@ import java.util.Map;
 
 class MessageSenderImplTest {
     @Test
-    void test_bad_ip(){
+    void test_ru_ip(){
         LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
         GeoService geoService = Mockito.mock(GeoServiceImpl.class);
         Location location = new Location("Moscow", Country.RUSSIA, null, 0);
         Mockito.when(geoService.byIp("172.123.12.19")).thenReturn(location);
         Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
         MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
-
         Map<String, String> headers = new HashMap<String, String>();
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
         Assertions.assertEquals("Добро пожаловать", messageSender.send(headers));
+
+    }
+
+    @Test
+    void test_en_ip(){
+        GeoService geoService = Mockito.mock(GeoServiceImpl.class);
+        LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
+        Location location = new Location("New York", Country.USA, " 10th Avenue", 32);
+        Mockito.when(geoService.byIp("96.44.183.149")).thenReturn(location);
+        Mockito.when(localizationService.locale(Country.USA)).thenReturn("Welcome");
+        Map<String, String> headers = new HashMap<String, String>();
+        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.44.183.149");
+        Assertions.assertEquals("Welcome", messageSender.send(headers));
     }
 }
